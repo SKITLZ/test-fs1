fa2d008cb1d71590038a606061314aa7e27d5d26
SKITLZ <gskitlz@gmail.com>
2020-03-25 01:44:31 +0300

    Change default server port to 5000

--- server/src/main.ts
+++ server/src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.use(express.static(CLIENT_FILES));
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
